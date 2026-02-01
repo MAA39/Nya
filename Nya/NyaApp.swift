3182bb98b8b7ddcae8a9c59a8e493ac288fcd0f9
MAA39 <masa.nekoshinshi39@gmail.com>
2026-02-01 22:14:06 +0900

    Initial commit: Xcode project setup

---
+++ Nya/NyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NyaApp.swift
+//  Nya
+//
+//  Created by maa on 2026/02/01.
+//
+
+import SwiftUI
+
+@main
+struct NyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NyaTests/NyaTests.swift
@@ -0,0 +1,17 @@
+//
+//  NyaTests.swift
+//  NyaTests
+//
+//  Created by maa on 2026/02/01.
+//
+
+import Testing
+@testable import Nya
+
+struct NyaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
